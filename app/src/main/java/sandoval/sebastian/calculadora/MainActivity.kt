505937266a81be540362f7d13c9c5d3677102371
sebassandoc <sebassandoc@gmail.com>
2025-01-30 17:46:52 -0700

    codigo inicial}

--- app/src/main/java/sandoval/sebastian/calculadora/MainActivity.kt
+++ app/src/main/java/sandoval/sebastian/calculadora/MainActivity.kt
@@ -1,12 +1,19 @@
 package sandoval.sebastian.calculadora
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    var operation: String = ""
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +23,40 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val bt0: Button = findViewById(R.id.bt0)
+        val bt1: Button = findViewById(R.id.bt1)
+        val bt2: Button = findViewById(R.id.bt2)
+        val bt3: Button = findViewById(R.id.bt3)
+        val bt4: Button = findViewById(R.id.bt4)
+        val bt5: Button = findViewById(R.id.bt5)
+        val bt6: Button = findViewById(R.id.bt6)
+        val bt7: Button = findViewById(R.id.bt7)
+        val bt8: Button = findViewById(R.id.bt8)
+        val bt9: Button = findViewById(R.id.bt9)
+        val btAdd: Button = findViewById(R.id.btAdd)
+        val btSubstract: Button = findViewById(R.id.btSubstract)
+        val btMultiply: Button = findViewById(R.id.btMultiply)
+        val btDivide: Button = findViewById(R.id.btDivide)
+        val btClear: Button = findViewById(R.id.btClear)
+        val btResult: Button = findViewById(R.id.btResult)
+        val Screen: TextView = findViewById(R.id.Screen)
+
+        bt1.setOnClickListener {
+            operation+="1"
+            Screen.setText(operation)
+        }
+        bt2.setOnClickListener {
+            operation+="2"
+            Screen.setText(operation)
+        }
+        bt3.setOnClickListener {
+            operation+="3"
+            Screen.setText(operation)
+        }
+        bt4.setOnClickListener {
+            operation+="4"
+            Screen.setText(operation)
+        }
+
     }
 }
